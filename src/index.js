4714c25765cf458c7289d11ee7c00f2fde840381
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-07 21:23:08 -0300

    code cleanup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ const { DEFAULT_SERVER, DEFAULT_CHANNEL, DISCORD_TOKEN } = require("./config.jso
 
 const client = new Client();
 
-// variables
-let ready = false;
-let current_channel = DEFAULT_CHANNEL;
-
 // start
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -15,61 +11,18 @@ client.on('ready', () => {
 
 // on message
 client.on('message', async (message) => {
-	if (message.content.toLocaleLowerCase().includes("(react)")) {
-		await message.react("❤");
-		await message.react("👍");
-		await message.react("👎");
-		await message.react("😂");
-		await message.react("😮");
-		await message.react("😡");
+	if (!DEFAULT_SERVER || DEFAULT_SERVER === message.guild.id) {
+		if (!DEFAULT_CHANNEL || DEFAULT_CHANNEL === message.channel.id) {
+			if (message.content.toLocaleLowerCase().includes("(react)")) {
+				await message.react("❤");
+				await message.react("👍");
+				await message.react("👎");
+				await message.react("😂");
+				await message.react("😮");
+				await message.react("😡");
+			}
+		}
 	}
 });
-
-function setChannelByName(name) {
-	const channel = getChannelByName(name);
-	if (channel) {
-		current_channel = channel;
-	}
-	console.log("Now you are on:", `#${channel.name}`, channel.id);
-}
-
-function setChannelByID(id) {
-	const channel = getChannelByID(id);
-	if (channel) {
-		current_channel = channel;
-	}
-	console.log("Now you are on:", `#${channel.name}`, channel.id);
-}
-
-async function say(message) {
-	if (!ready) return;
-	return await current_channel.send(message);
-}
-
-async function sayOnID(channelID, message) {
-	if (!ready) return;
-	return await getChannelByID(channelID).send(message);
-}
-
-async function sayOn(channel, message) {
-	if (!ready) return;
-	return await getChannelByName(channel).send(message);
-}
-
-function getEmojiByName(name) {
-	return client.emojis.find(emoji => emoji.name == name);
-}
-
-function getChannelByID(id) {
-	return client.channels.find(channel => channel.id == id);
-}
-
-function getChannelByName(name) {
-	return client.channels.find(channel => channel.name == name);
-}
-
-function getVoiceChannelByName(name) {
-	return client.voiceConnections.find(channel => channel.channel.name == name);
-}
 
 client.login(DISCORD_TOKEN);
